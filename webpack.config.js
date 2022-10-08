57c7c2a0713359034577214b6cc9c2e5476e509a
tonny187 <tonyoanthony@gmail.com>
2022-10-07 19:11:02 -0500

    inicialization of the project, install webpack and some loaders

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+    entry: './src/index.js',
+    output:{
+        path:path.resolve(__dirname, 'dist'),
+        filename:'bundle.js'
+    },
+    mode:'development',
+    resolve:{
+        extensions:['.js','.jsx']
+    },
+    module:{
+        rules:[
+            {
+                test:/\.(js|jsx)$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader'
+                }
+            },
+            {
+                test:/\.html$/,
+                use:[
+                    {
+                        loader:'html-loader'
+                    }
+                ]
+            },
+            {
+               test: /\.css$/,
+               use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                },
+                'css-loader'
+               ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./public/index.html',
+            filename:'./index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css'
+        })
+    ],
+    devServer:{
+        allowedHosts:path.join(__dirname,'dist'),
+        compress:true,
+        port:3005
+    }
+}
